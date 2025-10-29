f4670f97c3c043983e33a41bc18f93b47479d8f7
Silje32 <msh070425@gmail.com>
2025-10-29 14:40:28 +0100

    Added all my crud functions as comments in moviesController.js

---
+++ src/controllers/moviesController.js
@@ -0,0 +1,29 @@
+/* Lag CRUD-endepunkter */
+
+/* 
+const getAllMovies = (req, res) => {
+    // Hent alle filmer fra databasen
+};
+
+const getMovieById = (req, res) => {
+    // Hent en spesifikk film basert på ID
+};
+
+const postMovie = (req, res) => {
+    // Legg til en ny film i databasen
+};
+
+const putMovie = (req, res) => {
+    // Rediger en film
+}
+
+const postReview = (req, res) => {
+    // Legg inn en review
+}
+
+const getReviewsByMovieId = (req, res) => {
+    // Hent alle reviews for en spesifikk film
+}
+*/
+
+/* Når man har laget en controller må man også lage en route */

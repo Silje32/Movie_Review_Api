9dc94a578dff7b38887cb34d633e6e0cd26c69c5
Silje32 <msh070425@gmail.com>
2025-10-29 22:43:39 +0100

    Added reviewController.js with comments to my project

---
+++ src/controllers/reviewController.js
@@ -0,0 +1,21 @@
+/* Lag CRUD-endepunkter */
+
+/*
+const postReview = (req, res) => {
+    // Legg inn en review
+}
+
+const getReviewsByMovieId = (req, res) => {
+    // Hent alle reviews for en spesifikk film
+}
+*/
+
+/* 
+module.exports =
+{
+    postReview,
+    getReviewsByMovieId
+};  
+*/
+
+/* Når man har laget en controller må man også lage en route */

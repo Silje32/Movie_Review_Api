a6630df34a71147b4f45a93aa8383188a5fe2a0a
Silje32 <msh070425@gmail.com>
2025-10-29 23:12:34 +0100

    Added imports, create express app, middleware, routes, PORT variable and app.listen to start the server

--- src/app.js
+++ src/app.js
@@ -1,6 +1,25 @@
-/*  
-   Lag et Movie Review API slik at både filmelskere og kritikere kan 
-   finne, dele og oppdatere filmanmeldelser.
-*/
+// imports
+const express = require("express");
+const bodyParser = require("body-parser");
+const moviesRoutes = require("./routes/movies.js");
+const reviewsRoutes = require("./routes/reviews.js");
 
-/* Main entry? */
+// create express app
+const app = express();
+
+// middleware
+app.use(bodyParser.json());
+
+// routes
+app.use("/movies", moviesRoutes);
+app.use("/reviews", reviewsRoutes);
+
+const PORT = process.env.PORT || 3500;
+app.listen;
+
+// start server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+module.exports = app;

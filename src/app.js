907f487c2df0708c98d7afda228e935291efb13d
Silje32 <msh070425@gmail.com>
2025-11-04 11:28:50 +0100

    Created my Movies and Reviews tables in app.js

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const moviesRoutes = require("./routes/movies.js");
 const reviewsRoutes = require("./routes/reviews.js");
+const db = require();
 
 // create express app
 const app = express();
@@ -16,6 +17,26 @@ app.use("/routes/reviews", reviewsRoutes);
 
 const PORT = process.env.PORT || 3500;
 app.listen;
+
+// Create Movies table
+db.prepare("DROP TABLE IF EXISTS Movies").run();
+
+db.prepare(
+  `CREATE TABLE Movies
+(
+  `
+).run();
+console.log("Migration completed: Movies table created.");
+
+// Create Reviews table
+db.prepare("DROP TABLE IF EXISTS Reviews").run();
+
+db.prepare(
+  `CREATE TABLE Reviews
+(
+ `
+).run();
+console.log("Migration completed: Reviews table created.");
 
 // start server
 app.listen(PORT, () => {

46703d20069bcc840ccf47ba1ef0a6d6fb16a9be
Silje32 <msh070425@gmail.com>
2025-11-05 16:14:08 +0100

    Added comments and cleaned up my code

--- src/routes/reviews.js
+++ src/routes/reviews.js
@@ -1,11 +1,13 @@
-const express = require("express");
-const router = express.Router();
-
-// Controller functions
+// imports
+// first party imports
 const {
   postReviews,
   getReviewsByMovieId,
 } = require("../controllers/reviewsController");
+
+// third party imports
+const express = require("express");
+const router = express.Router();
 
 // Define routes
 router("/:id")

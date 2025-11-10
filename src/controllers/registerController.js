8a8b30c8eac703b80da3ce6d134048ec999bbcca
Silje32 <msh070425@gmail.com>
2025-11-10 13:05:02 +0100

    Made a handleNewUser function and hashed my password

---
+++ src/controllers/registerController.js
@@ -0,0 +1,35 @@
+// imports
+const bcrypt = require("bcrypt");
+const path = require("path");
+const fsPromises = require("fs").promises;
+
+// Database
+/* 
+const usersDB = {
+  users: require("../model/users.json"),
+  SetUsers: function (data) {
+    this.users = data;
+  },
+};
+*/
+const handleNewUser = async (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res
+      .status(400)
+      .json({ message: "Username and password are required." });
+    const duplicate = usersDB.users.find((person) => person.username === user);
+    if (duplicate) return res.sendStatus(409);
+    try {
+      const hashedPwd = await bcrypt.hash(password, 10);
+      const newUser = {
+        username: user,
+        password: hashedPwd,
+      };
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  }
+
+  module.exports = { handleNewUser };
+};

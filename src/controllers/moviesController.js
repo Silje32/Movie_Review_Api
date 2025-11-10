9e5b9af76c94bf1b9692005b427b5359bfcd44f9
Silje32 <msh070425@gmail.com>
2025-11-10 10:56:02 +0100

    Finished my putMovie function, added CRUD comments to my code and fixed my post request

--- src/controllers/moviesController.js
+++ src/controllers/moviesController.js
@@ -1,7 +1,7 @@
 /* Lag CRUD-endepunkter */
 const { seedMoviesTable } = require("../main.sqlite.js");
 
-// Hent alle filmer fra databasen
+// Hent alle filmer fra databasen (Read)
 const getAllMovies = (req, res) => {
   try {
     const newMovie = seedMoviesTable();
@@ -15,7 +15,7 @@ const getAllMovies = (req, res) => {
   }
 };
 
-// Hent en spesifikk film basert på ID.
+// Hent en spesifikk film basert på ID. (Read)
 const getMovieById = (req, res) => {
   const { id } = req.params;
 
@@ -33,11 +33,11 @@ const getMovieById = (req, res) => {
   }
 };
 
-// Legg til en ny film i databasen.
+// Legg til en ny film i databasen. (Create)
 const postMovie = (req, res) => {
   const { title, director, release_year, genre } = req.body;
 
-  if ((!title || !director, !release_year, !genre)) {
+  if (!title || !director || !release_year || !genre) {
     return res
       .status(400)
       .json({ error: "Title, director, release year and genre are required." });
@@ -68,36 +68,26 @@ const postMovie = (req, res) => {
     });
   }
 
-  // Rediger en film
+  // Rediger en film (Update).
   const putMovie = (req, res) => {
+    const { id } = req.params;
     const { title, director, release_year, genre } = req.body;
+
     // Når en film blir oppdatert: 200 OK eller 204 No Content.
-    if ((!title || !director, !release_year, !genre)) {
-      return res.status(200).json({
-        error: "Title, director, release year and genre are updated.",
-      });
+    if (!title || !director || !release_year || !genre) {
+      return res.status(200).json({ error: "OK" });
     }
     try {
-      const newMovie = seedMoviesTable({
-        title,
-        director,
-        release_year,
-        genre,
-      });
-      res.status(201).json({
-        message: "Movie created successfully.",
-        movie: {
-          id: newMovie.lastInsertRowid,
-          title,
-          director,
-          release_year,
-          genre,
-        },
-      });
+      const result = updateMovie(id, { title, director, release_year, genre });
+
+      if (result.changes === 0) {
+        return res.status(204).json({ error: "No content." });
+      }
+      res.json({ message: "Movie updated successfully." });
     } catch (error) {
-      console.error("Error creating movie:", error.message);
+      console.error("Database error:", error.message);
       res.status(500).json({
-        error: "An error occurred while creating the movie.",
+        error: "Failed to update movie.",
       });
     }
   };

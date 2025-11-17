dad35374301ac6bfde7424f7b3e8f8444b185c8b
Silje32 <msh070425@gmail.com>
2025-11-17 14:44:30 +0100

    Cleaned up my code

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -22,22 +22,22 @@ const authorizeNewUser = async (req, res) => {
   ({ message: "Unauthorized" });
   const match = await bcrypt.compare(password, foundUser.password);
   if (match) {
+    const accessToken = jwt.sign(
+      { username: foundUser.username },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "30m" }
+    );
+
+    const refreshToken = jwt.sign(
+      { username: foundUser.username },
+      process.env.REFRESH_TOKEN_SECRET,
+      { expiresIn: "1d" }
+    );
+
     res.json({ success: `User ${username} logged in!` });
   } else {
     res.sendStatus(401).json({ message: "Unauthorized" });
   }
-
-  const accessToken = jwt.sign(
-    { username: foundUser.username },
-    process.env.ACCESS_TOKEN_SECRET,
-    { expiresIn: "30m" }
-  );
-
-  const refreshToken = jwt.sign(
-    { username: foundUser.username },
-    process.env.REFRESH_TOKEN_SECRET,
-    { expiresIn: "1d" }
-  );
 
   res.json({ success: `User ${username} logged in!` });
 };

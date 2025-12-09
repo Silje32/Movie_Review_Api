c1b64afcb1f8b1967683eb8bb82c19c9054427e2
Silje32 <msh070425@gmail.com>
2025-12-09 15:11:40 +0100

    Finished with seeding the review tabel

--- seeding.js
+++ seeding.js
@@ -3,7 +3,7 @@ const db = Database("database.db", { verbose: console.log });
 db.pragma("foreign_keys = ON");
 
 /* Seeding Movies table */
-db.prepare(
+/* db.prepare(
   `INSERT INTO Movies (Id, Title, Director, Release_year, Genre) 
      VALUES (?, ?, ?, ?, ?)`
 ).run("1", "The Shawshank Redemption", "Frank Darabont", 1994, "Drama");
@@ -85,12 +85,13 @@ db.prepare(
 ).run("10", "Interstellar", "Christopher Nolan", 2014, "Sci-Fi"); */
 
 /* Seeding Reviews table */
-db.prepare(
+/* db.prepare(
   `INSERT INTO Reviews (id, movieId, reviewAuthor, reviewText, rating)
       VALUES (?, ?, ?, ?, ?)`
 ).run("1", "1", "Alice", "An amazing movie about hope and friendship.", 8);
 
-const reviewValues = [
+ */ const reviewValues = [
+  /*
   [
     "2",
     "1",
@@ -145,16 +146,106 @@ const reviewValues = [
   [
     "11",
     "4",
-    "Jane",
+    "Chris",
     "Pulp Fiction is comic and grim, sun-soaked and shadowy, tender and blunt.",
     6,
   ],
   [
     "12",
     "4",
-    "Jane",
+    "Marius",
     "Every element of the bizarre, dizzying journey is carefully conceived and executed to take its viewers into the darkest recesses of life, then, with a wink and a laugh, set them free.",
     6,
+  ],
+  ["13", "5", "Amalie", "It´s the greatest movie ever made!.", 10],
+  [
+    "14",
+    "5",
+    "Sofie",
+    "Forrest is a character who teaches by being. He doesn´t try to change people. He simply stays kind. In a world full of noise, his presence is what stays with you. His story isn´t about triumph in the traditional sense, it´s about resilience, and about loving people even when they don´t love you back the way you expect.",
+    10,
+  ],
+  [
+    "15",
+    "5",
+    "Alex",
+    "The movie teaches you so much about life and the meaning of it. That life isn´t as bad as most people make it seem. That an innocent man can impact so many lives with his innocence. The meaning of the movie is that everyone needs to have a better outlook on life.",
+    10,
+  ], */
+  [
+    "17",
+    "6",
+    "Amanda",
+    "Delivers twists that fit the evolving context of the story it´s creating and commandingly wins your attention enough to not turn you off. While it may not seem like it, there is a point and a light at the end of the tunnel to this maze.",
+    5,
+  ],
+  [
+    "18",
+    "6",
+    "Eric",
+    "This film must be revisited, talked about, analyzed, and rewatched again and again. It will surely grow upon each viewing, but it proves instantly enthralling the first time",
+    6,
+  ],
+  ["19", "6", "Gabriel", "Confusing nightmare.", 1],
+  ["20", "7", "Nora", "The Matrix is spectacular.", 10],
+  [
+    "21",
+    "7",
+    "David",
+    "The Matrix is a magical and fantastic showcase of how to make a visually stunning action film. I don´t think you can find any better movie in this genre. The actors performance are OK, but where they win your heart is by being cool and tough",
+    10,
+  ],
+  ["22", "7", "Andreas", "Hot damn what a good movie!.", 10],
+  [
+    "23",
+    "8",
+    "Johannes",
+    "Solid acting, amazing direction, and elaborate production design make Fight Club a wild ride.",
+    7,
+  ],
+  ["24", "8", "Lena", "BANGER film, nuff said.", 9],
+  [
+    "25",
+    "8",
+    "Steffen",
+    "Pitt dominates the screen every second he´s on it, and it simultaneously represents his weirdest, funniest and most charismatic role of his career.",
+    5,
+  ],
+  [
+    "26",
+    "9",
+    "Marius",
+    "A blood and thunder finale that takes the viewer into a conflict of (quite literally) Middle-Earth shattering dimensions. If anything, Peter Jackson and his collaborators try to do too much with the final book of J.R.R. Tolkien´s fantasy trilogy",
+    5,
+  ],
+  [
+    "27",
+    "9",
+    "Betina",
+    "Peter Jackson´s place in cinematic history is cemented with The Return of the King and now Star Wars seems like a film made a long time ago in a galaxy far, far away.",
+    10,
+  ],
+  [
+    "28",
+    "9",
+    "Eivind",
+    "Viggo Mortensen´s Aragorn is the good king of the title, and while the actor may look fetching in a crown and cloak, he doesn´t have half of the gravitas of Sean Bean´s Boromir in the first film.",
+    8,
+  ],
+  ["29", "10", "Jørgen", "One of the best sci-fi movies ever made.", 10],
+  [
+    "30",
+    "10",
+    "Emil",
+    "Humbling and epic in scope, designed and conceptualised brilliantly, but a tad too stand-off-ish emotionally. While the father-daughter dynamic works in parts, the Cooper-Brand relationship is never given the right treatment and collapses.",
+    5,
+  ],
+  [
+    "31",
+    "10",
+    "Mariann",
+    "Scientists will debate, theologians will contemplate, philosophers will wonder, and cinema lovers will bask in the glory of another remarkable Christopher Nolan achievement.",
+    8,
   ],
 ];
 
